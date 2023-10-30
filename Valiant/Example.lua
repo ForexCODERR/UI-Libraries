473abbd829434cb0e217451742d6c0c5947137b4
ForexCODERR <147941246+ForexCODERR@users.noreply.github.com>
2023-10-29 20:22:25 -0700

    Update Example.lua

--- Valiant/Example.lua
+++ Valiant/Example.lua
@@ -7,28 +7,28 @@ local Tab = Window:CreateTab("Scripts")
 local Page = Tab:CreateFrame("Page 1")
 
 Button = Page:CreateButton("Button", "Description", function()
-CreateNotification("Title", "Description", function(value)
-if value == true then
-print(true)
-else
-print(false)
-end
-end)
+    CreateNotification("Title", "Description", function(value)
+        if value == true then
+            print(true)
+        else
+            print(false)
+        end
+    end)
 end)
 
 Toggle = Page:CreateToggle("Toggle", "Description", function(arg)
-Toggle:UpdateToggle("New Title", "New Description")
-print(arg)
+   Toggle:UpdateToggle("New Title", "New Description")
+   print(arg)
 end)
 
 Bind = Page:CreateBind("KeyBind", "F", function(arg)
-Bind:UpdateBind("New Title")
-print(arg)
+   Bind:UpdateBind("New Title")
+   print(arg)
 end)
 
 TextBox = Page:CreateBox("TextBox", 10044538000, function(arg)
-TextBox:UpdateBox("New Title")
-print(arg)
+   TextBox:UpdateBox("New Title")
+   print(arg)
 end)
 
 Page:CreateSlider("Slider", 16, 500,function(arg)
